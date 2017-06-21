36432563bc0f76f49087b083b440aafd9e65d200
SHIGAYUYA <shiga-yuya-yr@ynu.jp>
2017-06-22 00:46:57 +0900

    SF用に変更

--- src/MapChipEdit.java
+++ src/MapChipEdit.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
@@ -13,53 +14,102 @@ import javax.swing.JOptionPane;
 
 
 public class MapChipEdit {
-	static BufferedImage mapChipImags[] = null;
-	static int chipX[] = null;
-	static int chipY[] = null;
-	public static void main(String args[]){
+	BufferedImage mapChipImags[] = null;
+	int chipX[] = null;
+	int chipY[] = null;
+	int width;
+	int height;
 
+	public static void main(String args[]){
 		BufferedReader ibr = null;
 		try {
-			ibr = new BufferedReader(new FileReader("map/chip.txt"));
+			ibr = new BufferedReader(new FileReader("mapData/folder.txt"));
 			String line = ibr.readLine();
+			int folderNum = -1;
+			if(line.indexOf("folderData") >= 0){
+				folderNum = Integer.parseInt(line.split(" ", 0)[1]);
+			}else{//ファイルエラー
+				JOptionPane.showMessageDialog(null, "エラー folderData");
+				System.exit(0);
+			}
 
-			if(line.indexOf("chipData") >= 0){
-			int imgNum = Integer.parseInt(line.split(" ", 0)[1]);
-			mapChipImags = new BufferedImage[imgNum];
-			chipX = new int[imgNum];
-			chipY = new int[imgNum];
-		}else{	//ファイルエラー
-			JOptionPane.showMessageDialog(null, "エラー");
-			System.exit(0);
-		}
-			String x_y[];
-			for (int i = 0; i < mapChipImags.length; i++) {
-			mapChipImags[i] = ImageIO.read(new File("map/"+ i + ".png"));
-			x_y = ibr.readLine().split(" ", 0);
-			chipX[i] = Integer.parseInt(x_y[0]);
-			chipY[i] = Integer.parseInt(x_y[1]);
-		}
+			for (int i = 0; i < folderNum; i++) {
+				new MapChipEdit(ibr.readLine());
+			}
 			ibr.close();
 		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null, "エラー");
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "エラー folderData");
+			System.exit(0);
+		}
+	}
+
+	public MapChipEdit(String folder){
+		System.out.println(folder+"作成開始....");
+		BufferedReader ibr = null;
+		String line = null;
+		try {
+			ibr = new BufferedReader(new FileReader("mapData/" + folder + "Chip.txt"));
+			line = ibr.readLine();
+			String datas[];
+			if(line.indexOf("chipData") >= 0){
+				datas = line.split(" ", 0);
+				int imgNum = Integer.parseInt(datas[1]);
+				mapChipImags = new BufferedImage[imgNum];
+				chipX = new int[imgNum];
+				chipY = new int[imgNum];
+				width = Integer.parseInt(datas[2]);
+				height = Integer.parseInt(datas[3]);
+			}else{	//ファイルエラー
+				JOptionPane.showMessageDialog(null, "エラー "+ folder);
+				System.exit(0);
+			}
+
+			for (int i = 0; i < mapChipImags.length; i++) {
+				datas = ibr.readLine().split(" ", 0);
+				line = datas[0];
+				mapChipImags[i] = ImageIO.read(new File("sf-matome20051117/" + folder + "/" + datas[0] + ".png"));
+				chipX[i] = Integer.parseInt(datas[1]);
+				chipY[i] = Integer.parseInt(datas[2]);
+			}
+			ibr.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "エラー " + line);
 			System.exit(0);
 		}
 
-		int total = 0;
-		for(int i = 0; i < chipX.length; i++){
-			total = total + chipX[i] * chipY[i];
+
+		BufferedImage bmpImg = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+		Graphics2D bg = (Graphics2D)bmpImg.createGraphics();
+		for(int i =0; i < mapChipImags.length; i++){
+			bg.drawImage(mapChipImags[i], chipX[i], chipY[i], null);
 		}
-
-		BufferedImage img = new BufferedImage(256 * 16, 256 * 16, BufferedImage.TYPE_3BYTE_BGR);
-
-		for(int i =0; i < total; i++){
-
-			drawChip(img.createGraphics(), 16 * (i % 256), 16 *(i / 256),  i);
+		
+		BufferedImage pngImg = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
+		Graphics2D pg = (Graphics2D)pngImg.createGraphics();
+		int c;
+		for(int i =0; i < width; i++){
+			for(int j = 0; j < height; j++){
+				c = bmpImg.getRGB(i, j);
+				
+				//System.out.println(Integer.toHexString(c));
+				if(c == 0xFF00FF00){
+					pg.setColor(new Color(1.0f, 1.0f, 1.0f, 0.0f));
+				}else{
+					pg.setColor((new Color(c)));
+				}
+				pg.drawLine(i, j, i, j);
+			}
 		}
+		
 		OutputStream out;
 		try {
-			out = new FileOutputStream("map/map.bmp");
-			ImageIO.write(img, "bmp", out);
+			out = new FileOutputStream("map/" + folder + "Map.bmp");
+			ImageIO.write(bmpImg, "bmp", out);
+			out.close();
+			out = new FileOutputStream("map/" + folder + "Map.png");
+			ImageIO.write(pngImg, "png", out);
 			out.close();
 		} catch (FileNotFoundException e) {
 			// TODO 自動生成された catch ブロック
@@ -68,15 +118,6 @@ public class MapChipEdit {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
-
-	}
-	public static void drawChip(Graphics2D g, int x, int y, int chipID){
-		int i = 0;
-		while(chipID >= (chipX[i] * chipY[i])){
-			chipID = chipID - (chipX[i] * chipY[i]);
-			i++;
-		}
-		g.drawImage(mapChipImags[i], x, y, x + 16, y + 16,
-				(chipID % chipX[i]) * 16, (chipID / chipX[i]) * 16, (chipID % chipX[i] + 1) * 16, (chipID / chipX[i] + 1) * 16, null);
+		System.out.println(folder+"作成終了");
 	}
 }
